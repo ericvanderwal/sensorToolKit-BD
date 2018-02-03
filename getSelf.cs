de9ac31db3782fa1db178fbaa15ca1eb6bb3828d
Eric Vander Wal <eric_vanderwal@hotmail.com>
2018-02-03 20:24:57 +0800

    Add files via upload

---
+++ getSelf.cs
@@ -0,0 +1,45 @@
+﻿// Dumb Game Dev 2018
+// Eric Vander Wal
+
+using UnityEngine;
+using Opsive.ThirdPersonController;
+
+namespace BehaviorDesigner.Runtime.Tasks.Basic.UnityBehaviour
+{
+	[TaskCategory("Custom")]
+	[TaskDescription("Get own game object (self).")]
+    public class getSelf : Action
+    {
+
+	   [Tooltip("Save self to this game object.")]
+        public SharedGameObject targetGameObject;
+
+        public override TaskStatus OnUpdate()
+        {
+
+            if (targetGameObject == null)
+            {
+                return TaskStatus.Failure;
+            }
+
+	        targetGameObject.Value = this.transform.gameObject;
+	        
+	        if(targetGameObject.Value == this.transform.gameObject)
+	        {
+		        targetGameObject.Value = this.transform.gameObject;
+		        return TaskStatus.Success;
+
+	        }
+	        
+	        return TaskStatus.Running;
+
+        }
+
+
+        public override void OnReset()
+        {
+            targetGameObject = null;
+
+        }
+    }
+}
