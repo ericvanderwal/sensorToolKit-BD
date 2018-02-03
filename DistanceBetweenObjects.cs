2d0da76bac35b83faf618a7f675ec800d68d9a87
Eric Vander Wal <eric_vanderwal@hotmail.com>
2018-02-03 20:32:00 +0800

    Add files via upload

---
+++ DistanceBetweenObjects.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using BehaviorDesigner.Runtime.Tasks.Movement;
+using Tooltip = BehaviorDesigner.Runtime.Tasks.TooltipAttribute;
+
+namespace BehaviorDesigner.Runtime.Tasks.DGD
+{
+	[TaskCategory("Custom")]
+	[TaskDescription("Get the distance between two game objects.")]
+	public class DistanceBetweenObjects : Action
+    {
+	    
+	    [Tooltip("First game object.")]
+	    public SharedGameObject m_FirstGameobject;
+	    
+	    [Tooltip("Second game object.")]
+	    public SharedGameObject m_SecondGameobject;
+	    
+	    [Tooltip("Distance between the two objects as a float.")]
+	    public SharedFloat m_distance;
+
+	    public override TaskStatus OnUpdate()
+        {
+
+	        if(m_FirstGameobject.Value == null || m_SecondGameobject.Value == null) 
+	        {
+			return TaskStatus.Failure;
+	        }
+	        
+	        else
+	        {
+	        	
+	        	m_distance.Value = Vector3.Distance(m_FirstGameobject.Value.transform.position, m_SecondGameobject.Value.transform.position);
+	        	return TaskStatus.Success;
+	        	
+	        }
+
+            return TaskStatus.Running;
+        }
+        
+	    public override void OnReset()
+	    {
+
+		    m_SecondGameobject = null;
+		    m_FirstGameobject = null;
+		    m_distance = null;
+	    }
+    }
+}
